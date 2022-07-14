d3b7dc5894c3ea5f51229ff4f9aa230099178030
Sachin Patil <sachinpatil01.2009@gmail.com>
2022-07-14 16:51:18 +0530

    Google Analytics added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,41 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import {AfterViewInit, Component, OnDestroy} from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 import * as firebase from 'firebase';
 
-
+import {Subscription} from 'rxjs';
+import {filter} from 'rxjs/operators';
+declare var gtag: Function;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  
+// export class AppComponent { 
 
-  title = 'product-catalogue';
- 
-  constructor(private router: Router) {}
+//   title = 'product-catalogue'; 
+//   constructor(private router: Router) {}
 
-  ngOnInit() {
-    this.router.navigate(['product'])
-  }
-  
+//   ngOnInit() {
+//     this.router.navigate(['product'])
+//   }  
+//   }
+
+  export class AppComponent implements AfterViewInit, OnDestroy {
+    private routerSubscription: Subscription;
+    constructor(private router: Router) {
+    }
+    ngOnInit() {
+      this.router.navigate(['product'])
+    }
+    ngAfterViewInit(): void {
+      // subscribe to router events and send page views to Google Analytics
+      this.routerSubscription = this.router.events
+        .pipe(filter(event => event instanceof NavigationEnd))
+        .subscribe((event: NavigationEnd) => {
+          gtag('config', 'UA-343565455–1', {'page_path': event.urlAfterRedirects});
+        });
+    }
+    ngOnDestroy(): void {
+      this.routerSubscription.unsubscribe();
+    }
   }
